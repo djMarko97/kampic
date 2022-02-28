88d9ced8419fef380f4de9910a20d3e81d9ca24b
Marko <djordjevic.marko373@gmail.com>
2022-02-28 14:49:29 +0100

    Add new and create basic

--- app.js
+++ app.js
@@ -23,20 +23,37 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
+
+// prikaz svih
 app.get('/campgrounds', async (req, res) => {
     const camps = await Campground.find({});
     res.render('campgrounds/index', { camps })
 })
 
+// ubacivanje novog
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const camp = new Campground(req.body.campground);
+    await camp.save();
+    res.redirect(`/campgrounds`);
+})
+
+// prikaz odredjenog
 app.get('/campgrounds/:id', async (req, res) => {
     const id = req.params.id;
     const camp = await Campground.findById(id);
     res.render('campgrounds/show', { camp })
 })
+
 
 
 app.listen(3000, () => {
