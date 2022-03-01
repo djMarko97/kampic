19aaa5d4af89584074afa5ef95b13e956354ee45
Marko <djordjevic.marko373@gmail.com>
2022-03-01 09:34:12 +0100

    basic update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -24,6 +25,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -54,6 +56,17 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render('campgrounds/show', { camp })
 })
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const id = req.params.id;
+    const camp = await Campground.findById(id);
+    res.render('campgrounds/edit', { camp })
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect('/campgrounds');
+})
 
 
 app.listen(3000, () => {
